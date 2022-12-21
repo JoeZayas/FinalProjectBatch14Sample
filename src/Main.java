1dc4fd312c2e443c26624f82664395b6547589ec
joezayas <joedza4610@gmail.com>
2022-12-21 16:53:28 -0600

    first commit

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,18 @@ public class Main {
         solar.moon=2;
         solar.planets=2;
         solar.sun=1;
+
+        FeatureSun sun = new FeatureSun();
+        sun.color="green";
+        sun.size="2349857";
+        sun.temp="235";
+
+        Planet1 planet = new Planet1();
+        planet.color="red";
+        planet.size=34;
+        planet.temp="3546";
+        planet.rings="4";
+
     }
+
 }

f477ec8ffecb20214872451643aaf326cb32af2f
joezayas <joedza4610@gmail.com>
2022-12-21 16:40:42 -0600

    ready to go

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        SolarSystem solar = new SolarSystem();
+        solar.moon=2;
+        solar.planets=2;
+        solar.sun=1;
+    }
+}
